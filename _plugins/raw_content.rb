67563871f4e24ce8e5486c22115755ab5f794755
GaryNg <garyng.zhongbo@gmail.com>
2018-07-23 05:45:19 +0800

    feat: add reveal.js

---
+++ _plugins/raw_content.rb
@@ -0,0 +1,14 @@
+module Jekyll
+
+    class RawContent < Generator
+
+        def generate(site)
+            site.collections.each do |collection|
+                collection[1].each do |post|
+                    post.data['raw_content'] = Liquid::Template.parse(post.content).render(@context)
+                end
+            end
+        end
+    end
+
+end
